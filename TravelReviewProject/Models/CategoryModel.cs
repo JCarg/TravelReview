3d502dae81e52fc7b98b410a2ec5a91e03f3eb28
JCarg <Jcarg108@gmail.com>
2017-02-10 13:53:55 -0500

    Models Added

---
+++ TravelReviewProject/Models/CategoryModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelReviewProject.Models
+{
+    public class CategoryModel
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<ReviewModel> Reviews { get; set; }
+    }
+}
---
+++ TravelReviewProject/Models/ReviewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TravelReviewProject.Models
+{
+    public class ReviewModel
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Summary { get; set; }
+        public string Title { get; set; }
+        public string Comment { get; set; }
+        public string Location { get; set; }
+        public DateTime PublishDate { get; set; }
+        public DateTime VacationStart { get; set; }
+        public DateTime VacationEnd { get; set; }
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual CategoryModel Category { get; set; }
+    }
+}
